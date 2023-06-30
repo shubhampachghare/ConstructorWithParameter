3871a897c95331e191f4212e4e2ea5725d0fea4c
shubhampachghare <shubhampachghare@gmail.com>
2023-07-01 02:22:08 +0530

    [Shubham] -Constructor

---
+++ Constructor/Constructor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAsics
+{
+    class Constructor
+    {
+        public string Model ;
+        public string Color;
+        public int Year;
+
+        public Constructor (string Model,string Color, int Year)
+        {   
+           this.Model = Model;
+            this.Color = Color;
+            this.Year = Year;
+        }
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,13 @@
+﻿using BAsics;
+
+namespace AConstructor
+{
+     class Program
+     {
+        static void Main(string[] args)
+        {
+            Constructor Add = new Constructor("i10","Red",2023);
+            Console.WriteLine("Car Name is : "+ Add.Model +" Color is "+Add.Color+" Year of Manufacture "+Add.Year);               
+        }
+     }
+}
